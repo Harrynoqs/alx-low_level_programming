053404b98f3d922edf34fe445bd0b2df652be582
Harrynoqs <noqsnoqs@gmail.com>
2022-12-15 05:34:57 -0800

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+}
