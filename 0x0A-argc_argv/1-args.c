439c0ab411ffd4aa834fa81270fa4d5aabd7a87a
Harrynoqs <noqsnoqs@gmail.com>
2023-01-09 11:07:42 -0800

    0x0A. C - argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
-* main - prints the name number of arguments passed to it
-* @argc: number of arguments passed to the function
-* @argv: argument vector of pointers to strings
-* Return: always 0
+* main - program that prints its name, followed by a new line
+*
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0
 */
-
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-printf("%d\n", argc - 1);
-return (0);
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,42 +1,22 @@
-#include <stdio.h>
-		
 #include "main.h"
-		
+#include <stdio.h>
 
-		
 /**
-		
- * main - prints all arguments it receives
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
- *
-		
- * Return: Always 0 (Success)
-		
- */
-		
+* main - program that prints its name, followed by a new line
+*
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: 0
+*/
 int main(int argc, char *argv[])
-		
 {
-		
 	int i;
-		
 
-		
 	for (i = 0; i < argc; i++)
-		
 	{
-		
 		printf("%s\n", argv[i]);
-		
 	}
-		
-
-		
 	return (0);
-		
 }
 
