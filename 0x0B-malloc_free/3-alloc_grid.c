2e73092bf696a25fdb77680ca7a612ecd26837d1
Harrynoqs <noqsnoqs@gmail.com>
2023-01-11 10:45:31 -0800

    commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two string
+ *
+ * @width: width of grid
+ * @height: height of grid
+ *
+ * Return: a pointer that points to a newly allocated space which
+ * contains the contents of @s1, followed by the contents of @s2,
+ * and null terminated. Should return NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	
+}
