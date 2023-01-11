d1d6a51842ca13feda0086b272f78b38aa35d223
Harrynoqs <noqsnoqs@gmail.com>
2023-01-11 10:58:51 -0800

    commit

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int *array_range(int min, int max);
-void *_calloc(unsigned int nmemb, unsigned int size);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *malloc_checked(unsigned int b);
 int _putchar(char c);
-
-#endif
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
