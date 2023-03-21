700d429e9a3d75f2807897e717eed03d9de8758e
Harrison Njuguna <noqsnoqs@gmail.com>
2023-03-21 18:00:23 +0200

    commit

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -25,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif/*MAIN_H*/
